cb648bd82165500dc812f9ad0ac068bb44dd223c
Anand Raj Singh <singhanand31019@gmail.com>
2025-11-17 19:32:46 +0530

    add the nodemailer functionality

--- models/File.js
+++ models/File.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -16,6 +19,37 @@ const fileSchema = new mongoose.Schema({
 
     }
 });
+
+//post middleware
+fileSchema.post("save",async function (doc) { //doc refers to the entry saved in DB
+        try {
+            console.log("DOC:",doc);
+            console.log("MAIL_USER =", process.env.MAIL_USER);
+            console.log("MAIL_PASS =", process.env.MAIL_PASS ? "LOADED" : "NOT LOADED");
+
+
+            let transporter = nodemailer.createTransport({
+                host: process.env.MAIL_HOST,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASSWORD
+                }
+            })
+
+            //send mail
+            let info= await transporter.sendMail({
+                from:"Anand",
+                to:doc.email,
+                subject:"New File Uploaded on Cloudinary",
+                html:`<h2>Hello Jee</h2><p>File Uploaded</p>view here: <a href="#{doc.imageUrl}">${doc.imageUrl}</a>`,
+            });
+            console.log("Info:",info);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
